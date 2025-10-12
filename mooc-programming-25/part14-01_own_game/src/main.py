c60cce83285ab2720489828494e137f8ceb6ae86
Morgane Bona-Pellissier <morgane@bona-pellissier.net>
2025-10-12 21:00:25 +0200

    pygame

--- mooc-programming-25/part14-01_own_game/src/main.py
+++ mooc-programming-25/part14-01_own_game/src/main.py
@@ -1,19 +1,38 @@
-# Complete your game here
+# The robot (stuck to the mouse) must collect coins to gain points,
+# while avoiding the monsters (ghosts),
+# which can move either upwards, downwards, to the left or the the right.
+# In case a collision with a monster happens, the game ends.
+# Both the coins and the robots appear randomly.
+
+
 import pygame, random
+from random import randint, choice
 
+class Image():
+    def __init__(self, image):
+        self.image = pygame.image.load(image)
+        self.x = randint(0, 1280-self.image.get_width())
+        self.y = randint(0, 960-self.image.get_height())
 
-class Robot:
-    def __init__(self):
-        self.image = pygame.image.load("robot.png")
-        self.x = (1280 - self.image.get_width())/2
-        self.y = (960 - self.image.get_height())/2
-    
     def get_x_y(self):
         self.min_x = self.x
         self.min_y = self.y
         self.max_x = self.x + self.image.get_width()
         self.max_y = self.y + self.image.get_height()
         return self.min_x, self.min_y, self.max_x, self.max_y
+
+    def collision(self, image):
+        min_x, min_y, max_x, max_y = self.get_x_y()
+        Image.min_x, image.min_y, image.max_x, image.max_y = image.get_x_y()
+        
+        if image.min_x < min_x < image.max_x or image.min_x < max_x < image.max_x:
+            if image.min_y < min_y < image.max_y or image.min_y < max_y < image.max_y:
+                return True
+
+
+class Robot(Image):
+    def __init__(self):
+        super().__init__("robot.png")  
 
     def move(self, event):
         if event.type == pygame.MOUSEMOTION:
@@ -21,126 +40,90 @@ class Robot:
             self.y = event.pos[1]-self.image.get_height()/2
 
 
-class Monster():
+class Monster(Image):
     def __init__(self):
-        self.image = pygame.image.load("monster.png")
-        # Monster starts at a random position
-        self.x = random.randint(0, 1280-self.image.get_width())
-        self.y = random.randint(0, 590-self.image.get_height())
-        self.min_x = self.x
-        self.min_y = self.y
-        self.max_x = self.x + self.image.get_width()
-        self.max_y = self.y + self.image.get_height()
-                       
-        self.velocity = 1
-  
-    def appear(self):
-        if random.randint(0, 500) == 1:
+        super().__init__("monster.png")
+                     
+        self.velocity = randint(1,3)
+        self.direction = choice(["left", "right", "up", "down"])
+        if self.direction in ["left", "right"]:
+            self.x = 0
+        else:
+            self.y = 0
+
+    @staticmethod
+    def appear():
+        if random.randint(0, 600) == 1:
             return Monster()
 
-"""
     def move(self):
-        #to be done
-        self.y += self.velocity
+        min_x, min_y, max_x, max_y = self.get_x_y()
 
-    def rebound(self):
-        #to be done
-        if self.y + self.image.get_height() == 960:
-            exit()
+        if self.direction == "left":
+            self.x -= self.velocity
+            if min_x <= 0:
+                self.direction = "right"
+        elif self.direction == "right":
+            self.x += self.velocity
+            if max_x >= 1280:
+                self.direction = "left"
+        elif self.direction == "up":            
+            self.y -= self.velocity
+            if min_y <= 0:
+                self.direction = "down"
+        elif self.direction == "down":         
+            self.y += self.velocity
+            if max_y >= 960:
+                self.direction = "up"
 
-    def collision(self, robocop: Robot):
-        #to be done
-        max_y = self.y + self.image.get_height()
-        max_x = self.x + self.image.get_width()
-        min_x = self.x
-        robot_top = robocop.y
-        if max_y >= robot_top:
-            if robocop.get_left() < max_x < robocop.get_right() or robocop.get_left() < min_x < robocop.get_right():
-                self.displayed = False
-                return True
-"""
 
-class Coin():
+class Coin(Image):
     def __init__(self):
-        self.image = pygame.image.load("coin.png")
-        # Coin appears at random x and y positions  
-        self.limit_x = 1280 - self.image.get_width()
-        self.limit_y = 960 - self.image.get_height()
-        self.x = random.randint(0, self.limit_x)
-        self.y = random.randint(0, self.limit_y)    
+        super().__init__("coin.png")
 
-        self.collected = False
-
-    def get_x_y(self):
-        self.min_x = self.x
-        self.min_y = self.y
-        self.max_x = self.x + self.image.get_width()
-        self.max_y = self.y + self.image.get_height()
-        return self.min_x, self.min_y, self.max_x, self.max_y
-
-    def appear(self):
-        if random.randint(0, 500) == 1:
+    @staticmethod
+    def appear():
+        if random.randint(0, 400) == 1:
             return Coin()
-
-    def collect(self, robot: Robot):
-        self.min_x, self.min_y, self.max_x, self.max_y = self.get_x_y()
-        robot.min_x, robot.min_y, robot.max_x, robot.max_y = robot.get_x_y()
-        
-        print(f"{robot.min_x} < {self.min_x} < {robot.max_x} or {robot.min_x} < {self.max_x} < {robot.max_x}")
-        if robot.min_x < self.min_x < robot.max_x or robot.min_x < self.max_x < robot.max_x:
-            print("test_x ok")
-            print(f"{robot.min_y} < {self.min_y} < {robot.max_y} or {robot.min_y} < {self.max_y} < {robot.max_y}")
-            if robot.min_y < self.min_y < robot.max_y or robot.min_y < self.max_y < robot.max_y:
-                print("collected")
-                self.collected = True
-        
-    def disappear(self):
-        pass
 
 
 class Game:
     def __init__(self):
         self.points = 0
         self.robot = Robot()
-        self.monster = Monster()
         self.monsters = []
-        self.coin = Coin()
         self.coins = []
-        #self.monster = Monster()
 
         pygame.init()
         self.window = pygame.display.set_mode((1280, 960))
         pygame.display.set_caption("Morgane's Game")
 
         self.main_loop()
-
-
-    """
-
-    def collision(self, robot: Robot, monster: Monster):
-        max_y_robot = robot.y + robot.image.get_height()
-        max_x_robot = robot.x + robot.image.get_width()
-        min_x = self.x
-        robot_top = robot.y
-        if max_robot_y >= robot_top:
-            if robot.get_left() < max_x < robot.get_right() or robot.get_left() < min_x < robot.get_right():
-                exit()
-    """
-      
-        
+            
     def main_loop(self):
         while True:
             for event in pygame.event.get():
                 self.robot.move(event)
                 if event.type == pygame.QUIT:
                     exit()
-            monster = self.monster.appear()
+
+            # Monsters
+            monster = Monster.appear()
             if monster:
                 self.monsters.append(monster)
-            coin = self.coin.appear()
+            for m in self.monsters:
+                m.move()
+                if m.collision(self.robot):
+                    exit()
+            
+            # Coins
+            coin = Coin.appear()
             if coin:
                 self.coins.append(coin)
-            self.coin.collect(self.robot)
+            for c in self.coins:
+                c.collision(self.robot)
+            
+            # Display image
             self.draw_window()
 
     def draw_window(self):
@@ -150,9 +133,9 @@ class Game:
             self.window.blit(m.image, (m.x, m.y))
         for c in self.coins:
             self.window.blit(c.image, (c.x, c.y))
-            if c.collected:
+            if c.collision(self.robot):
                 self.coins.remove(c)
-                self.point += 1
+                self.points += 1
         
         #Points
         game_font = pygame.font.SysFont("Arial", 24)
